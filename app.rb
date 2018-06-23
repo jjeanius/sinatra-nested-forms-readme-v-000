ccf487a50eec6301bab7d8a48fecad89df67fe07
Judy McGoldrick <jjeanius@verizon.net>
2018-06-23 03:02:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,4 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  post '/student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do|detials|
+      Course.new(detiails)
+    end
+
+   @courses = :student
 end
